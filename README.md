3da8c18abed02bcbef915b62c7b958a4dec22dd9
Joe Dytrych <me@jdytrych.co.uk>
2013-08-29 13:17:28 +0000

    Add install script

---
+++ README.md
@@ -0,0 +1,6 @@
+Social Shake Source
+===================
+
+To install on Ubuntu:
+
+`sudo curl -L https://github.com/Craig88/Social_Shake_Source/raw/master/install.sh | sh`
---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+git clone https://github.com/Craig88/Social_Shake_Source.git
+
+sudo apt-get update
+sudo apt-get install -y libfreetype6 libfreetype6-dev 
+sudo apt-get install -y libpng12-0 libpng12-dev
+sudo apt-get install -y graphviz graphviz-dev
+sudo apt-get install -y python-pip python-dev
+
+sudo pip install networkx
+sudo pip install pyparsing==1.5.7
+sudo pip install
+sudo pip install Matplotlib
+
+cd Social_Shake_Source
+mkdir Output
