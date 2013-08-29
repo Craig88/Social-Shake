37637f78cbd85ce902b2d4d5321c862b45f5c185
Joe Dytrych <me@jdytrych.co.uk>
2013-08-29 14:40:30 +0100

    Added pydot

--- install.sh
+++ install.sh
@@ -7,10 +7,16 @@ sudo apt-get install -y libfreetype6 libfreetype6-dev
 sudo apt-get install -y libpng12-0 libpng12-dev
 sudo apt-get install -y graphviz graphviz-dev
 sudo apt-get install -y python-pip python-dev
+sudo apt-get install -y cowsay
+
+export PATH=$PATH:/usr/games/
+
+alias say=cowsay
 
 sudo pip install networkx
 sudo pip install pyparsing==1.5.7
 sudo pip install numpy
+sudo pip install pydot
 sudo pip install Matplotlib
 
 cd Social_Shake_Source
