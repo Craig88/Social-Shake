0a3291df8dda1b446122cc3879efd1d391c5d833
Craig Steele <cr@igsteele.com>
2013-09-11 09:54:28 +0100

    updated the macinstall

---
+++ macinstall.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+git clone https://github.com/Craig88/Social_Shake_Source.git
+
+brew update
+brew install freetype 
+brew install libpng
+brew install graphviz
+brew install pip
+
+export PATH=$PATH:/usr/games/
+
+sudo pip install networkx
+sudo pip install pyparsing==1.5.7
+sudo pip install numpy
+sudo pip install pydot
+sudo pip install Matplotlib
+
+cd Social_Shake_Source
+mkdir Output
